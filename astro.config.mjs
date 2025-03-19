5ba785ff85b60ef788b71f3399d0fab714636fcb
Miguel <31miguelop31@gmail.com>
2025-03-19 17:14:09 -0500

    modificacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://migueljcm16.github.io',
-    base: 'star-planet',
+    site: 'https://starplanet.com',
 });
